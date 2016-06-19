5c5b6ade9bdf3ad494951e03618c60d77ae284a3
Archer <421228286@qq.com>
2016-06-19 15:30:22 +0800

    在windowManager上显示View

--- app/src/main/java/com/example/archer/mobliesafe/service/AddressService.java
+++ app/src/main/java/com/example/archer/mobliesafe/service/AddressService.java
@@ -5,9 +5,13 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.Color;
+import android.graphics.PixelFormat;
 import android.os.IBinder;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
+import android.view.WindowManager;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.archer.mobliesafe.db.dao.AddressDao;
@@ -23,6 +27,8 @@ public class AddressService extends Service {
     private TelephonyManager tmService;
     private MyListener mylistener;
     private OutCallReceiver receiver;
+    private WindowManager mWM;
+    private TextView view;
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -57,9 +63,15 @@ public class AddressService extends Service {
                     System.out.println("电话铃响");
 //根据来电号码，查询归属地
                     String address = AddressDao.getAddress(incomingNumber);
-                    Toast.makeText(AddressService.this, address, Toast.LENGTH_LONG).show();
+//                    Toast.makeText(AddressService.this, address, Toast.LENGTH_LONG).show();
+                   showToast(address);
                     break;
 
+                case TelephonyManager.CALL_STATE_IDLE://电话闲置状态
+                    if (mWM!=null&&view!=null){
+                    mWM.removeView(view);//windows中移除界面
+                }
+                    break;
                 default:
                     break;
             }
@@ -80,7 +92,8 @@ public class AddressService extends Service {
             String resultData = getResultData();//返回去电的号码
 
             String address = AddressDao.getAddress(resultData);
-            ToastUtils.showToast(context,address);
+//            ToastUtils.showToast(context,address);
+            showToast(address);
         }
     }
 
@@ -93,4 +106,33 @@ public class AddressService extends Service {
 
 
     }
+/**
+ * 自定义归属地的浮窗
+ *
+ */
+
+    private void showToast(String text){
+        //使用windowManager可以在其他第三方APP中运用上弹出浮窗
+        mWM = (WindowManager) this
+                .getSystemService(Context.WINDOW_SERVICE);
+
+        WindowManager.LayoutParams mParams = new WindowManager.LayoutParams();
+
+        mParams.height = WindowManager.LayoutParams.WRAP_CONTENT;
+        mParams.width = WindowManager.LayoutParams.WRAP_CONTENT;
+        mParams.format = PixelFormat.TRANSLUCENT;
+
+        mParams.type = WindowManager.LayoutParams.TYPE_TOAST;
+        mParams.setTitle("Toast");
+        mParams.flags = WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+                | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                | WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE;
+
+
+        view = new TextView(this);
+        view.setText(text);
+        view.setTextColor(Color.RED);
+        mWM.addView(view, mParams);//将view添加到屏幕
+    }
+
 }
