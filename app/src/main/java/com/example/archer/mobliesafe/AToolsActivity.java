240d1d88843e0ac97223b88e8faef07ee0ab5ecb
Archer <421228286@qq.com>
2016-10-25 09:36:44 +0800

    完成动态读取 危险权限的 代码工作 （android 6.0 以后）

--- app/src/main/java/com/example/archer/mobliesafe/AToolsActivity.java
+++ app/src/main/java/com/example/archer/mobliesafe/AToolsActivity.java
@@ -1,14 +1,25 @@
 package com.example.archer.mobliesafe;
 
+import android.Manifest;
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.Toast;
+
 import com.example.archer.mobliesafe.utils.SmsUtils;
 
 
@@ -32,6 +43,7 @@ import com.lidroid.xutils.view.annotation.ViewInject;
  */
 public class AToolsActivity  extends Activity {
 
+    private static final int WRITE_EXTERNAL_STORAGE_CODE = 1;
     private ProgressDialog progressDialog;
     @ViewInject(R.id.progressBar)
     private ProgressBar progressBar;
@@ -41,6 +53,7 @@ public class AToolsActivity  extends Activity {
 
         setContentView(R.layout.activity_atools);
         ViewUtils.inject(this);
+        requestPermission();
     }
 
     /**
@@ -141,4 +154,53 @@ progressDialog.setMax(count);
         }.start();
     }
 
+    private void requestPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            // 第一次请求权限时，用户如果拒绝，下一次请求shouldShowRequestPermissionRationale()返回true
+            // 向用户解释为什么需要这个权限
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+                new AlertDialog.Builder(this)
+                        .setMessage("申请读取SD卡的权限")
+                        .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                //申请相机权限
+                                ActivityCompat.requestPermissions(AToolsActivity.this,
+                                        new String[]{Manifest.permission.CAMERA}, WRITE_EXTERNAL_STORAGE_CODE);
+                            }
+                        })
+                        .show();
+            } else {
+                //申请相机权限
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, WRITE_EXTERNAL_STORAGE_CODE);
+            }
+        } else {
+//            tvPermissionStatus.setTextColor(Color.GREEN);
+//            tvPermissionStatus.setText("相机权限已申请");
+            ToastUtils.showToast(AToolsActivity.this,"读取SD卡权限已经申请");
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == WRITE_EXTERNAL_STORAGE_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                tvPermissionStatus.setTextColor(Color.GREEN);
+//                tvPermissionStatus.setText("相机权限已申请");
+                ToastUtils.showToast(AToolsActivity.this,"读取SD卡权限已经申请");
+            } else {
+                //用户勾选了不再询问
+                //提示用户手动打开权限
+                if (!ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                    Toast.makeText(this, "读取SD卡权限已经申请", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
 }
+
+
+
